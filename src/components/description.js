e2d1815fd39ac3ca81cfbfcef53ff0dd69cd6334
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-05-02 18:07:46 +0200

    Update description.js

--- src/components/description.js
+++ src/components/description.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { useStateValue } from '../state';
-import Link from './link';
 
 export default function Description() {
   const [{ config }] = useStateValue();
