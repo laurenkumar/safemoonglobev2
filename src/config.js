ebb329bf243a22550d77e240fb8d6bd9f883d136
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-05-02 16:02:34 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,11 +6,11 @@
 export default {
   keyword: 'safemoon + safemoon price + safemoon news',
   globeBackgroundTexture:
-    'https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/background.png',
+    'https://metoorising.withgoogle.com/assets/images/background-blended2.jpg',
   globeCloudsTexture:
-    'https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/clouds.png',
+    'https://metoorising.withgoogle.com/assets/images/weather.jpg',
   globeTexture:
-    'https://raw.githubusercontent.com/chrisrzhou/react-globe/main/textures/globe_dark.jpg',
+    'https://metoorising.withgoogle.com/assets/images/earth.jpg',
   options: {
     ambientLightColor: '#44b394',
     ambientLightIntensity: 1,
